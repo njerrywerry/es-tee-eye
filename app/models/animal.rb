8e19b9c8068d30601706c39426d02712a3404cdb
Sylvia <sylviakieha@gmail.com>
2016-01-08 00:14:45 +0300

    populating the models with logic

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,15 @@
 class Animal < ActiveRecord::Base
+  belongs_to :tribe
+  self.inheritance_column = :race
+
+  # a way to know which animals
+  # will subclass the Animal model
+  def self.races
+    %w(Lion WildBoar Meerkat)
+  end
+
 end
+
+class Lion < Animal; end
+class Meerkat < Animal; end
+class WildBoar < Animal; end
